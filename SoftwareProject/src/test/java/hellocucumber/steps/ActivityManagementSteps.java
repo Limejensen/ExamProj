f3102a7c1243c1a681ad34c1a6fb2efa0c553ec6
ateroh <asger.otte@gmail.com>
2025-05-04 14:44:09 +0200

    Update ActivityManagementSteps.java
    some stuff

--- SoftwareProject/src/test/java/hellocucumber/steps/ActivityManagementSteps.java
+++ SoftwareProject/src/test/java/hellocucumber/steps/ActivityManagementSteps.java
@@ -2,6 +2,8 @@ package hellocucumber.steps;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDate;
+
 import dtu.example.ui.Activity;
 import dtu.example.ui.Project;
 import dtu.example.ui.Schedule;
@@ -22,25 +24,23 @@ public class ActivityManagementSteps {
 
     @When("I create a new activity {string} in the project with projectID {string}")
     public void iCreateANewActivityInTheProjectWithProjectID(String string, String string2) {
-        
-        throw new io.cucumber.java.PendingException();
+        activity = new Activity(string);
+        project.addActivity(activity);
     }
 
     @When("set a budget of {int} hours")
     public void setABudgetOfHours(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        activity.setBudgetedHours(hours);
     }
 
     @When("set the start date to {string} and end date to {string}")
     public void setTheStartDateToAndEndDateTo(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        activity.setStartDate(LocalDate.parse(string));
+        activity.setEndDate(LocalDate.parse(string2));
     }
     
     @Then("the activity should be added to the project")
     public void theActivityShouldBeAddedToTheProject() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assertTrue(project.getActivities().contains(activity));
     }
 }
